0ffb0bdae8ce13e5ee2c54e0632acda83fc5297d
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-07-27 13:42:11 +0300

    testing strategies

---
+++ cpp/deciphering-oop/ch15_testing/student.h
@@ -0,0 +1,42 @@
+// Purpose: To illustrate Student.h header file
+
+#ifndef _STUDENT_H
+#define _STUDENT_H
+
+#include "person.h"
+
+class Student : public Person {
+private:
+  // data members
+  float gpa = 0.0; // in-class initialization
+  string currentCourse;
+  const char
+      *studentId; // Again, we have one pointer data member to demonstrate deep copy / assignment
+  static int numStudents;
+
+public:
+  // member function prototypes
+  Student(); // default constructor
+  Student(const string &, const string &, char, const char *, float, const string &, const char *);
+  Student(const Student &); // copy constructor
+  Student(Student &&);      // move copy constructor
+  ~Student() override;      // virtual destructor
+  void EarnPhD();
+  // inline function definitions
+  float GetGpa() const { return gpa; }
+  const string &GetCurrentCourse() const { return currentCourse; }
+  const char *GetStudentId() const { return studentId; }
+  void SetCurrentCourse(const string &); // prototype only
+
+  void Print() const override;
+  void IsA() const override;
+  // note: we choose not to redefine Person::Greeting(const Student &) const
+  Student &operator=(const Student &); // overloaded assignment operator prototype
+  Student &operator=(Student &&);      // overloaded move assignment operator prototype
+
+  static int GetNumberStudents();
+};
+
+inline void Student::SetCurrentCourse(const string &c) { currentCourse = c; }
+
+#endif
